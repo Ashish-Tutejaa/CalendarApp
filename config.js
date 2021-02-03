8c6b465a5e4e5acd4e9d345ecd0322cbf06dcacf
Ashish <ashish.tutejaa@gmail.com>
2021-02-04 00:57:56 +0700

    config added.

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports =  {
+    secret : "tutejaislit",
+    mongo_uname:'tuteja',
+    mongo_pass:'Ashish101@doge',
+    mongo_db:'MERN',
+    host : 'localhost',
+    user : 'root',
+    password : 'Ashish101@doge',
+    database : 'calendarapp'
+}
